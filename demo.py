8af34dd2b6174224eac1f193fe477b8efa0c410f
yupeifengyannis <yupeifengyannis@foxmail.com>
2023-05-07 13:28:51 +0800

    feat(*):compatible with version 1.6.0 and above of pytorch

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
+import os
 import torch
-from torch.autograd import Variable
+#from torch.autograd import Variable
 import utils
 import dataset
 from PIL import Image
@@ -8,32 +9,40 @@ import models.crnn as crnn
 
 
 model_path = './data/crnn.pth'
-img_path = './data/demo.png'
+# 这里在原有的demo基础之上增加了多增加一张手写12345的图片
+img_path_list = ['./data/demo1.png', './data/demo.png']
 alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
 
 model = crnn.CRNN(32, 1, 37, 256)
 if torch.cuda.is_available():
     model = model.cuda()
-print('loading pretrained model from %s' % model_path)
-model.load_state_dict(torch.load(model_path))
+if os.path.exists(model_path):
+    print('loading pretrained model from %s' % model_path)
+    model.load_state_dict(torch.load(model_path))
 
 converter = utils.strLabelConverter(alphabet)
-
+# python的PIL image的resize方法传入的是(width, height)
 transformer = dataset.resizeNormalize((100, 32))
-image = Image.open(img_path).convert('L')
-image = transformer(image)
+image_list = []
+for img_path in img_path_list:
+    image = Image.open(img_path).convert('L')
+    image = transformer(image)
+    image = image.unsqueeze(0)
+    image_list.append(image)
+image = torch.cat(image_list, 0)
 if torch.cuda.is_available():
     image = image.cuda()
-image = image.view(1, *image.size())
-image = Variable(image)
 
 model.eval()
 preds = model(image)
 
-_, preds = preds.max(2)
-preds = preds.transpose(1, 0).contiguous().view(-1)
+# 这里的preds的大小是(26,1,37)，相当于将图片的在宽的方向上切分26个时间，每个时刻上会预测字符在37个字符上概率分布
 
-preds_size = Variable(torch.IntTensor([preds.size(0)]))
-raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
-sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
-print('%-20s => %-20s' % (raw_pred, sim_pred))
+_, preds = preds.max(2) # 这里其实是取用了第三维最大值的位置，即概率最大的字符位置
+preds = preds.transpose(1, 0).contiguous()
+
+for idx, preds in enumerate(preds):
+    preds_size = torch.IntTensor([preds.size(0)])
+    raw_pred = converter.decode(preds.data, preds_size.data, raw=True) # 这里是将空白符号也会解码出来的
+    sim_pred = converter.decode(preds.data, preds_size.data, raw=False) # 这里使用大beta函数来做映射
+    print('%-20s => %-20s' % (raw_pred, sim_pred))
